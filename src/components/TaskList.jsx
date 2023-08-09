79b7a18da07c7db083a011d2fab856e1b367cbd8
David Echenagucia <leoechena21@gmail.com>
2023-08-09 17:38:05 -0500

    primer comentario para subir a github

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,21 @@
+import TaskCard from "./TaskCard";
+
+function TaskList({ tasks , deleteTask}) {
+    if (tasks.length === 0){
+    return <h1>No hay tarea aun</h1>
+  }
+
+  return (
+  <div>
+    {tasks.map((task) => (
+      <TaskCard key={task.id} task={task} deleteTask={deleteTask} />
+    ))}
+  </div>
+  );
+}
+
+export default TaskList;
+
+
+/* import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react"; */
